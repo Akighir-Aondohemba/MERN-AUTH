f65741eca8a64b1d60920c1e308e7f14b3225c49
Aondohemba <akighirjoseph77@gmail.com>
2023-10-08 12:18:27 +0100

    add react router dom and create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 export default function App() {
-  return (
-    <h1>App</h1>
-  )
+  return <BrowserRouter>
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/sign-in" element={<Signin />} />
+  <Route path="/sign-up" element={<SignUp />} />
+  <Route path="/profile" element={<Profile />} />
+</Routes>
+  </BrowserRouter>;
 }
